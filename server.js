fd8a0763a3dcca2fc1090effcc3590a8a4b9ac10
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-07-01 12:39:16 -0500

    express con ejs

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 
+const posts = [];
+const pageSize = 5;
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
