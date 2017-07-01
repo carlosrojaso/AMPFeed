96c772aec1216e38fd732d5a4ee94f73693bbcff
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-07-01 13:18:17 -0500

    Cargando datos

---
+++ feed.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+curl -H "Content-Type: application/json" -X POST -d '{"videoID":"r2TyQLgYQok"}' http://localhost:3000/new
+curl -H "Content-Type: application/json" -X POST -d '{"videoID":"hl-LqSE-dA4"}' http://localhost:3000/new
+curl -H "Content-Type: application/json" -X POST -d '{"videoID":"Ik3pK-bZQPQ"}' http://localhost:3000/new
+curl -H "Content-Type: application/json" -X POST -d '{"videoID":"L5KKCebdRbA"}' http://localhost:3000/new
+curl -H "Content-Type: application/json" -X POST -d '{"videoID":"rN36uX-GorI"}' http://localhost:3000/new
+
+echo "Para utilizar paginacion"
+
+#curl -H "Content-Type: application/json" -X POST -d '{"videoID":"r2TyQLgYQok"}' http://localhost:3000/new
+#curl -H "Content-Type: application/json" -X POST -d '{"videoID":"hl-LqSE-dA4"}' http://localhost:3000/new
+#curl -H "Content-Type: application/json" -X POST -d '{"videoID":"Ik3pK-bZQPQ"}' http://localhost:3000/new
+#curl -H "Content-Type: application/json" -X POST -d '{"videoID":"L5KKCebdRbA"}' http://localhost:3000/new
+#curl -H "Content-Type: application/json" -X POST -d '{"videoID":"rN36uX-GorI"}' http://localhost:3000/new
